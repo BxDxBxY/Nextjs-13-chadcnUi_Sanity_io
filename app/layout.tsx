2eb82ff36657aae2e3993a98ab93e1ff738642ca
Jack <sunatila6391@email.com>
2023-10-21 17:40:57 -1200

    updated: logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,12 +7,10 @@ export const metadata: Metadata = {
   other: {
     "theme-color": "#c2c2c2",
     "color-theme": "dark only",
-    "twitter:image":
-      "https://avatars.mds.yandex.net/i?id=5efd2068a563aadd572b35fba114068c305da015-10850463-images-thumbs&n=13",
+    "twitter:image": "jack-logo.png",
     "twitter:card": "summary_large_image",
     "og:url": "jacks-nextjs13-project.rizz",
-    "og:image":
-      "https://avatars.mds.yandex.net/i?id=5efd2068a563aadd572b35fba114068c305da015-10850463-images-thumbs&n=13",
+    "og:image": "jack-logo.webp",
     "og:type": "website",
   },
 };
